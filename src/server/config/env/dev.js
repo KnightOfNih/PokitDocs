260adf162d0ec7e83d84547a7b3407f2d05887a6
Will Lovett <lovettwb@gmail.com>
2015-11-15 22:12:10 -0500

    Removing return url.

--- src/server/config/env/dev.js
+++ src/server/config/env/dev.js
@@ -8,9 +8,6 @@ module.exports = {
     getPort : function(){
         return port;
     },
-    getReturnUrl : function(){
-        return domain + ":" + port + "/auth/intuit/return";
-    },
     getDomain : function(){
         return domain + ":" + port;
     }
